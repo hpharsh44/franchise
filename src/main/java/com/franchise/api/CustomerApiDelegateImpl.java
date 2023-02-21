bd42248a5fcbc90765db850839a284d62fac4d6d
Dipen <40355743+hpharsh44@users.noreply.github.com>
2023-02-21 23:31:46 +0530

    Committed changes for close review comments
    Committed changes for close review comments

--- src/main/java/com/franchise/api/CustomerApiDelegateImpl.java
+++ src/main/java/com/franchise/api/CustomerApiDelegateImpl.java
@@ -3,6 +3,8 @@ package com.franchise.api;
 import com.franchise.model.CustomerOrder;
 import com.franchise.service.CustomerOrderService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
@@ -10,15 +12,22 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class CustomerApiDelegateImpl implements CustomerApiDelegate {
 
     private final CustomerOrderService customerOrderService;
 
     public ResponseEntity<List<CustomerOrder>> getAllCustomerOrder(Long customerId) {
-        return ResponseEntity.ok(customerOrderService.getAllCustomerOrder(customerId));
+      log.info("get all request received for customerId: ",customerId);
+        List<CustomerOrder> response = customerOrderService.getAllCustomerOrder(customerId);
+        log.info("get all request completed for customerId: ",customerId);
+        return ResponseEntity.ok(response);
     }
 
     public ResponseEntity<CustomerOrder> getCustomerOrderByOrderId(Long customerId, Long orderId) {
-        return ResponseEntity.ok(customerOrderService.getCustomerOrderByOrderId(customerId, orderId));
+        log.info("get order by  customerId and Order Id ",customerId,orderId);
+        CustomerOrder response = customerOrderService.getCustomerOrderByOrderId(customerId, orderId);
+        log.info("get order by  customerId and orderId completed",customerId,orderId);
+        return ResponseEntity.ok(response);
     }
 }
--- src/main/java/com/franchise/api/FranchiseApiDelegateImpl.java
+++ src/main/java/com/franchise/api/FranchiseApiDelegateImpl.java
@@ -5,20 +5,29 @@ import com.franchise.model.OrderRequest;
 import com.franchise.model.UpdateOrderStatusRequest;
 import com.franchise.service.FranchiseOrderService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class FranchiseApiDelegateImpl implements FranchiseApiDelegate {
 
     private final FranchiseOrderService franchiseOrderService;
 
     public ResponseEntity<CustomerOrder> placeOrder(OrderRequest orderRequest) {
-        return ResponseEntity.ok(franchiseOrderService.placeOrder(orderRequest));
+        log.info("PlaceOrder request received.");
+        CustomerOrder response = franchiseOrderService.placeOrder(orderRequest);
+        log.info("PlaceOrder request completed.");
+        return ResponseEntity.ok(response);
     }
 
     public ResponseEntity<String> updateOrderStatus(UpdateOrderStatusRequest updateOrderStatusRequest) {
-        return ResponseEntity.ok(franchiseOrderService.updateOrderStatus(updateOrderStatusRequest));
+        log.info("update order request received.");
+        String response = franchiseOrderService.updateOrderStatus(updateOrderStatusRequest);
+        log.info("update order request received.");
+        return ResponseEntity.ok(response);
     }
 }
